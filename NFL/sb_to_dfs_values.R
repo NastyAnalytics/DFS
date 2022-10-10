72cc5c8393ab35a50f667646a3d8ae55f78f9b30
Luke Hughes <112493241+NastyAnalytics@users.noreply.github.com>
2022-10-10 07:36:46 -0400

    Add files via upload

--- NFL/sb_to_dfs_values.R
+++ NFL/sb_to_dfs_values.R
@@ -10,8 +10,9 @@ library('cfbfastR')
 
 setwd("~/Documents/NFL")
 
-sb_names <- c('Mitchell Trubisky','Lamar Jackson (BAL)','Mark Ingram','Travis Etienne','Cedrick Wilson Receiving Yards','Michael Thomas (NO)','O.J. Howard Receiving Yards','A.J. Green (ARI)','Robby Anderson','Richie James','Gabriel Davis')
-dfs_names <-c('Mitch Trubisky', 'Lamar Jackson','Mark Ingram II','Travis Etienne Jr.','Cedrick Wilson Jr.','Michael Thomas','O.J. Howard','A.J. Green','Robbie Anderson','Richie James Jr.','Gabe Davis')
+sb_names <- c('Mitchell Trubisky','Lamar Jackson (BAL)','Mark Ingram','Travis Etienne','Cedrick Wilson Receiving Yards','Michael Thomas (NO)','O.J. Howard Receiving Yards','A.J. Green (ARI)','Robby Anderson','Richie James','Gabriel Davis',
+              'Josh Palmer','Keelan Cole')
+dfs_names <-c('Mitch Trubisky', 'Lamar Jackson','Mark Ingram II','Travis Etienne Jr.','Cedrick Wilson Jr.','Michael Thomas','O.J. Howard','A.J. Green','Robbie Anderson','Richie James Jr.','Gabe Davis','Joshua Palmer','Keelan Cole Sr.')
 clean_names <- data_frame(sb_names,dfs_names)
 colnames(clean_names) <- c('clean_names','player')
 
@@ -46,27 +47,29 @@ driver$open()
 
 
 #set URL
-url <- paste0("https://sportsbook.draftkings.com/leagues/football/nfl?category=passing-props&subcategory=pass-+-rush-yds")
+url <- paste0("https://sportsbook.draftkings.com/leagues/football/nfl?category=passing-props&subcategory=pass-yds")
 
 # navigate to an URL
 driver$navigate(url)
 Sys.sleep(1)
 
+
 #Getting Nodes
 html <- driver$getPageSource()[[1]]
+
 player <- read_html(html) %>%
   html_nodes(".sportsbook-row-name") %>% 
   html_text()
 player <- as.data.frame(player)
 
-ou <- read_html(html) %>%
+pa_ou <- read_html(html) %>%
   html_nodes("th+ .sportsbook-table__column-row .sportsbook-outcome-cell__line") %>% 
   html_text()
-ou <- as.numeric(ou)
-ou <- as.data.frame(ou)
+pa_ou <- as.numeric(pa_ou)
+pa_ou <- as.data.frame(pa_ou)
 
 #Combining Nodes
-pa_odds <- cbind(player,ou)
+pa_odds <- cbind(player,pa_ou)
 #close the driver
 driver$close()
 
@@ -85,7 +88,7 @@ driver$open()
 
 
 #set URL
-url <- paste0("https://sportsbook.draftkings.com/leagues/football/nfl?category=rush/rec-props&subcategory=rush-+-rec-yds")
+url <- paste0("https://sportsbook.draftkings.com/leagues/football/nfl?category=rush/rec-props&subcategory=rush-yds")
 
 # navigate to an URL
 driver$navigate(url)
@@ -98,14 +101,14 @@ player <- read_html(html) %>%
   html_text()
 player <- as.data.frame(player)
 
-ou <- read_html(html) %>%
+ru_ou <- read_html(html) %>%
   html_nodes("th+ .sportsbook-table__column-row .sportsbook-outcome-cell__line") %>% 
   html_text()
-ou <- as.numeric(ou)
-ou <- as.data.frame(ou)
+ru_ou <- as.numeric(ru_ou)
+ru_ou <- as.data.frame(ru_ou)
 
 #Combining Nodes
-ru_odds <- cbind(player,ou)
+ru_odds <- cbind(player,ru_ou)
 #close the driver
 driver$close()
 
@@ -136,31 +139,67 @@ player <- read_html(html) %>%
   html_text()
 player <- as.data.frame(player)
 
-ou <- read_html(html) %>%
+re_ou <- read_html(html) %>%
   html_nodes("th+ .sportsbook-table__column-row .sportsbook-outcome-cell__line") %>% 
   html_text()
-ou <- as.numeric(ou)
-ou <- as.data.frame(ou)
+re_ou <- as.numeric(re_ou)
+re_ou <- as.data.frame(re_ou)
 #Combining Nodes
-re_odds <- cbind(player,ou)
+re_odds <- cbind(player,re_ou)
 #close the driver
 driver$close()
 
 #close the server
 rD[["server"]]$stop()
 
+pa_odds <- left_join(pa_odds,ru_odds)
+pa_odds$ru_ou <- ifelse(is.na(pa_odds$ru_ou),0,pa_odds$ru_ou)
+pa_odds$ou <- pa_odds$pa_ou + pa_odds$ru_ou
+ru_odds <- left_join(ru_odds,re_odds)
+ru_odds$re_ou <- ifelse(is.na(ru_odds$re_ou),0,ru_odds$re_ou)
+ru_odds$ou <- ru_odds$ru_ou + ru_odds$re_ou
+
+pa_odds <- pa_odds[,-c(2,3)]
+ru_odds <- ru_odds[,-c(2,3)]
+re_odds <- rename(re_odds, ou = re_ou)
+
 
 pa_odds1 <- left_join(pa_odds, current_slate1)
-pa_odds1$value <- pa_odds1$ou / (pa_odds1$Salary/1000)
+pa_odds1$value <- pa_odds1$ou / (pa_odds1$Salary/2500)
 ru_odds1 <- left_join(ru_odds, current_slate1)
 ru_odds1$value <- ru_odds1$ou / (ru_odds1$Salary/1000)
 re_odds1 <- left_join(re_odds, current_slate1)
 re_odds1$value <- re_odds1$ou / (re_odds1$Salary/1000)
 
 pa_odds <- left_join(pa_odds, current_slate)
-pa_odds$value <- pa_odds$ou / (pa_odds$Salary/1000)
+pa_odds$value <- pa_odds$ou / (pa_odds$Salary/2500)
 ru_odds <- left_join(ru_odds, current_slate)
 ru_odds$value <- ru_odds$ou / (ru_odds$Salary/1000)
 re_odds <- left_join(re_odds, current_slate)
 re_odds$value <- re_odds$ou / (re_odds$Salary/1000)
+
+pa_odds1 <- pa_odds1[complete.cases(pa_odds1),]
+ru_odds1 <- ru_odds1[complete.cases(ru_odds1),]
+re_odds1 <- re_odds1[complete.cases(re_odds1),]
+pa_odds <- pa_odds[complete.cases(pa_odds),]
+ru_odds <- ru_odds[complete.cases(ru_odds),]
+re_odds <- re_odds[complete.cases(re_odds),]
+
+setwd("~/Documents/NFL/Slates")
+
+write.csv(pa_odds,'DK QB values showdown 10 09 22.csv')
+write.csv(ru_odds,'DK RB values showdown 10 09 22.csv')
+write.csv(re_odds,'DK WR values showdown 10 09 22.csv')
+write.csv(pa_odds1,'FD QB values showdown 10 09 22.csv')
+write.csv(ru_odds1,'FD RB values showdown 10 09 22.csv')
+write.csv(re_odds1,'FD WR values showdown 10 09 22.csv')
+
+
+
+
+
+
+
+
+
 
